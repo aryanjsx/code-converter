7223efc2b8f07eb9cd5c256df209cd325f851e24
Aryan Kumar <kumararyan1929@gmail.com>
2025-11-23 17:57:32 +0530

    updated name

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC = () => {
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>
         </div>
         <div>
-          <h1 className="text-xl font-bold text-white tracking-tight">CodeMorph</h1>
+          <h1 className="text-xl font-bold text-white tracking-tight">CodexConvert</h1>
           <p className="text-xs text-gray-400 font-medium">Multi-Language Converter</p>
         </div>
       </div>
